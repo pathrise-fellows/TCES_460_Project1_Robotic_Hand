25019914b73fc2639debcfbbf9a21d677cd9bdf2
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2018-01-12 21:24:36 -0800

    Add files via upload

--- Main_Program/hand.cpp
+++ Main_Program/hand.cpp
@@ -1,21 +1,28 @@
-
-
 #include <stdio.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 #include <mcp3004.h>
+#include <softPwm.h>
 
-
-int pressure_data[5];
+float pressure_data[5];
 int servo_data[6];
-
-int PWM_1 = 25;
-int PWM_2 = 24;
-int PWM_3 = 23;
-int PWM_4 = 22;
-int PWM_5 = 21;
-
 int PWM = {25,24,23,22,21};
+int R = 3220;
+int resistance[5];
+float pressure [5];
+float voltage[5];
+
+#define BASE 100
+#define SPI_CHAN 0
+
+
+
+void servo_setup(int size) {
+	for (int i=0; i<size; i++) {
+		pinMode(PWM[i],OUTPUT);
+		softPwmcreate(PWM[i],0,50);
+	}
+}
 
 void servo_write(int * data) {
 	
@@ -35,18 +42,56 @@ void pressure_read(int * data.int BASE) {
 
 
 
+void calc_voltage(int size) {
+	for (int i=0; i<size; i++) {
+		float voltage[i] = data[i]*(5.0)/1023.0;
+	}
+}
+
+void calc_resistance(int size) {
+	for (int i=0; i<size; i++) {
+		float resistance[i] = R*(5.0)/voltage[i] - 1.0);
+	}
+}
+void calc_pressure(int size) {
+	float fsrG = 1.0/Res;
+	for (int i =0; i<size; i++) {
+		if (resistance[i] <=600) {
+			pressure[i] = fsrG - 0.00075)/ 0.00000032639;
+		}
+		else {
+			pressure[i] = fsrG / 0.000000642857;
+		}
+	}
+}
+
+void calc(int size) {
+	for (int i =0; i<size; i++) {
+		resistance[i] = R*(5.0)/(data[i]*(5.0)/1023.0) - 1.0);
+		float fsrG = 1.0/resistance[i];
+		if (resistance[i] <=600) {
+			pressure[i] = fsrG - 0.00075)/ 0.00000032639;
+		}
+		else {
+			pressure[i] = fsrG / 0.000000642857;
+		}
+	}
+}
 int main() {
 	
 	wiringPiSetup();
+	mcp3004Setup(BASE,SPI_CHAN);
+	
 	/*
 	Order:
-	
+	Setup
+	while(1) {
 	Read Analog
 	Send Data
 	Receive Data
 	Calculate Data
 	Write to Servo
-	
+	}
 	*/
 	
 }
