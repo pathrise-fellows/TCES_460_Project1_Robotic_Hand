de33de7a6dbfb03631e596b713e546d29b62812b
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2018-01-18 14:57:13 -0800

    Update server.cc

--- Main_Program/server.cc
+++ Main_Program/server.cc
@@ -14,12 +14,7 @@
 
 #include <time.h>
 #include <sys/time.h>
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
-#include <mcp3004.h>
-#include <LSM9DS1.h>
-#include <LSM9DS1_Types.h>
-#include <softPwm.h>
+
 
 const int max_data_size = 4096;
 
@@ -44,19 +39,7 @@ float mag_array [3];
 float press_array [5] = {0.14,1.14,2.14,3.14,4.14};
 
 
-LSM9DS1 imu(IMU_MODE_I2C, 0x6b, 0x1e);
 
-int R_DIV = 47000;
-
-
-const float STR_R[5]= {14038,8000,8000,8000,8000};
-const float BEND_R[5] = {68000,40000,40000,40000,40000};
-const int PWM[5] = {25,24,23,22,21};
-float flex_data[5];
-float flex_voltage[5];
-float resistance[5];
-float buzzer_val[5];
-int servo_val[5];
 
 
 
@@ -159,78 +142,26 @@ void servo_write(int size) {
 	}
 }
 
-void flex_read(int base) {
-	for (int i=0; i<5; i++) {
-		flex_data[i] = analogRead(base+i);
-	}
-}
 
-void calc_voltage(int size) {
-	for (int i=0; i<size; i++) {
-		flex_voltage[i] = flex_data[i]*(5.0)/1023.0;
-	}
-}
-
-void calc_resistance(int size) {
-	for (int i=0; i<size; i++) {
-		resistance[i] = R_DIV*(5.0/flex_voltage[i] - 1.0);
-	}
-}
-
-void calc_angle(int size) {
-	for (int i=0; i<size; i++) {
-		flex_array[i] = map(resistance[i],STR_R[i],BEND_R[i],0,90.0);	
-	}
-}
-
-void calc_all(int size) {
-	for (int i=0; i<size; i++) {
-		flex_voltage[i] = flex_data[i]*(5.0)/1023.0;
-		resistance[i] = R_DIV*(5.0/flex_voltage[i] - 1.0);
-		flex_array[i] = map(resistance[i],STR_R[i],BEND_R[i],0,90.0);	
-	}
-}
-
-void imu_read() {
-		while (!imu.gyroAvailable()) ;
-        imu.readGyro();
-		gyro_array[0] = imu.calcGyro(imu.gx);
-		gyro_array[1] = imu.calcGyro(imu.gy);
-		gyro_array[2] = imu.calcGyro(imu.gz);
-        while(!imu.accelAvailable()) ;
-        imu.readAccel();
-		accel_array[0] = imu.calcAccel(imu.ax);
-		accel_array[1] = imu.calcAccel(imu.ay);
-		accel_array[2] = imu.calcAccel(imu.az);
-        while(!imu.magAvailable()) ;
-        imu.readMag();
-		mag_array[0] = imu.calcMag(imu.mx);
-		mag_array[1] = imu.calcMag(imu.my);
-		mag_array[2] = imu.calcMag(imu.mz);
-}
 int main(void){
-    GOOGLE_PROTOBUF_VERIFY_VERSION;
-    server_setup();
-    glove_setup();
-    glove_set();
+   	GOOGLE_PROTOBUF_VERIFY_VERSION;
+   	server_setup();
+    	glove_setup();
+    	glove_set();
 	wiringPiSetup();
-   	imu.begin();
-   	if (!imu.begin()) {
-        	fprintf(stderr, "Failed to communicate with LSM9DS1.\n");
-        	exit(EXIT_FAILURE);
-   	}
-    imu.calibrate();
+   	
 	
-	while (1){
+	for (int i=0; i<100; i++) {
 		send_data();
 		print_in();
 		receive();
+		delay(1000);
 	}
 	send_data();
-    print_in();
+    	print_in();
 	receive();
-    close(sock);
-    printf("done with server\n");
+    	close(sock);
+    	printf("done with server\n");
 	exit(EXIT_SUCCESS);
     return 0;
 }
