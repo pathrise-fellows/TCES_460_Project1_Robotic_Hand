543b046ef7eefd1a4ce9585d3a4631ae7d4b7def
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2018-01-24 14:26:53 -0800

    Update hand_server.cc

--- Test_with_client_server/hand_server.cc
+++ Test_with_client_server/hand_server.cc
@@ -97,8 +97,8 @@ int timeout_recvfrom(int timeoutinseconds){
     struct timeval t;
     FD_ZERO(&socks);
     FD_SET(sock, &socks);
-    tv.tv_sec = 0;
-    tv.tv_usec = timeoutinseconds * 1000;
+    t.tv_sec = 0;
+    t.tv_usec = timeoutinseconds * 1000;
     printf("if start\n", n);
     int temp = select(sock + 1, &socks, NULL, NULL, &t);
     printf("%d \n", temp);
